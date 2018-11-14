e396f024f5897ff777475ef3b5a5ac89e5007515
Jun Yu <johnyu617@yahoo.com>
2018-11-14 14:15:54 -0800

    Update main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("title: %s\n", title)
 	}
 
-	fmt.Fprintf(w, "Hello from:  "+title+"--- jun-test1 \n")
+	fmt.Fprintf(w, "Hello from:  "+title+"--- jun-test2 \n")
 }
 
 func main() {
